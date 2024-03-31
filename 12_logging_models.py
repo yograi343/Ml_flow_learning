e9a9a856758b16702902dc1717632fad419422e3
Yogesh Rai <yograi343@gmail.com>
2024-03-31 13:02:29 +0530

    this section we leant about autolog and how it logs all the log parameters and some default artifacts without we instructing for that you just need to write mlflow.autolog() thats it

---
+++ 12_logging_models.py
@@ -0,0 +1,36 @@
+import mlflow
+from mlflow_utils import get_mlflow_experiment
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    experiment = get_mlflow_experiment(experiment_name="testingmlflow1")
+    print(f"name: {experiment.name}")
+
+    with mlflow.start_run(
+        run_name="logging models", experiment_id=experiment.experiment_id
+    ) as run:
+
+        X, y = make_classification(
+            n_samples=1000,
+            n_features=10,
+            n_informative=5,
+            n_redundant=5,
+            random_state=42,
+        )
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42
+        )
+        mlflow.autolog()
+
+        rfc = RandomForestClassifier(n_estimators=100, random_state=42)
+        rfc.fit(X_train, y_train)
+        y_pred = rfc.predict(X_test)
+
+        print(f"run_id: {run.info.run_id}")
+        print(f"Experiment_Id: {run.info.experiment_id}")
